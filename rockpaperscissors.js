997fd428c4dac541938ae332d5673abef4e9346d
abelchac <abelchac@seas.upenn.edu>
2020-08-28 22:14:22 -0400

    show score

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,7 +9,8 @@ function computerPlay() {
     return MOVES[Math.floor(Math.random() * MOVES.length)];
 }
 let play = false;
-
+let humanPoints = 0;
+let compPoints = 0;
 function playRound(playerSelection, computerSelection){
     let player = MOVES.indexOf(playerSelection.toLowerCase());
     let computer = MOVES.indexOf(computerSelection.toLowerCase());
@@ -17,10 +18,12 @@ function playRound(playerSelection, computerSelection){
         return "This is bad move";
     }
     if((player - computer) == 1 || (player - computer) == -2){
+        humanPoints++;
         return "You Win! " + MOVESRET[player] + " beats " + MOVESRET[computer];
     } else if ((player - computer == 0)){
         return "You Tie! " + MOVESRET[player] + " Ties " + MOVESRET[computer];
     }else { 
+        compPoints++;
         return "You Lose! " + MOVESRET[computer] + " beats " + MOVESRET[player];
     }
     play = false;
@@ -31,11 +34,21 @@ let val = ""
 
 let i = 5;
 
+function setScore() {
+    let element;
+    element = document.getElementById("hscore");
+    element.innerHTML = humanPoints.toString();
+    element = document.getElementById("cscore");
+    element.innerHTML = compPoints.toString();
+
+}
+
 function game() {
-    
+    setScore();
     if(i > 0){
             console.log(playRound(val, computerPlay()));
-            val = "";        
+            val = "";       
+
     } else{
         console.log("There are no more rounds")
     } 
@@ -48,6 +61,7 @@ function buttonPress(event) {
     game();
 }
 
+setScore();
 buttonRock.addEventListener('click', buttonPress);
 buttonPaper.addEventListener('click', buttonPress);
 buttonScissors.addEventListener('click', buttonPress);	
